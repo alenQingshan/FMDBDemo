d566177a94564db040a92e49ae3a3b741c26d83a
alenQingshan <liuqingshan@liuqingshandemac-mini.local>
2016-05-18 09:40:28 +0800

    Initial Commit

---
+++ FMDBDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FMDBDemo
+//
+//  Created by 刘青山 on 16/5/18.
+//  Copyright © 2016年 刘青山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
